bd7404b1283d4768866570c164e0bd60266fe928
Sean Parker <seanrprkr@gmail.com>
2026-09-13 00:50:47 -0400

    set up local graphql env

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
-import { execute, parse } from "graphql";
-import { schema } from "./schema";
+import { createServer } from 'node:http';
+import { createYoga } from 'graphql-yoga';
+import { schema } from './schema';
 
-async function main() {
-  const myQuery = parse(`
-    query {
-      hello
-    }
-  `);
-
-  const result = await execute({
-    schema,
-    document: myQuery,
-  });
-
-  console.log(result);
+function main() {
+  const yoga = createYoga({ schema });
+  const server = createServer(yoga);
+  server.listen(4000, () => {
+    console.info('Server is running on http://localhost:4000/graphql')
+  })
 }
 
 main()
