18d3423373997a4d6e734ab905e169fd03a0c54a
Sean Parker <seanrprkr@gmail.com>
2026-09-15 00:19:49 -0400

    install sqllite through prisma

---
+++ src/script.ts
@@ -0,0 +1,30 @@
+// 1
+// after (Prisma 7)
+import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+
+// 2
+const adapter = new PrismaBetterSqlite3({ url: 'file:./prisma/dev.db' })
+const prisma = new PrismaClient({ adapter })
+
+// 3
+async function main() {
+  const allLinks = await prisma.link.findMany()
+  console.log(allLinks)
+}
+
+const newLink = await prisma.link.create({
+  data: {
+    description: 'Fullstack tutorial for GraphQL',
+    url: 'www.howtographql.com'
+  }
+})
+
+// 4
+main()
+  // 5
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
+  
